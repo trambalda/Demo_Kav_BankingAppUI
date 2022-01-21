588465963ad8247d6938615ff8c0ea3aa1a38a4f
Denis Rubtsov <trambalda@gmail.com>
2022-01-21 17:08:20 +0300

    Initial Commit

---
+++ Demo_Kav_BankingAppUI/Demo_Kav_BankingAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_Kav_BankingAppUIApp.swift
+//  Demo_Kav_BankingAppUI
+//
+//  Created by Денис Рубцов on 21.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Demo_Kav_BankingAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
